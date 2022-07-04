0f9fc26560e65188de09f9e47a077e0d043c3b10
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-07-04 22:46:35 +0530

    Time: 24 ms (73.16%), Space: 19.7 MB (13.12%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int len = nums.size();
+        map<int,int> m;
+        for(int i=0;i<len;i++){
+            m[nums[i]]+=1;
+        }
+        len = floor(len/2);
+        for(auto it: m){
+            if(it.second > len)
+                return it.first;
+        }
+        return 0;
+    }
+};
