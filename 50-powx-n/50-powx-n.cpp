6b219ae1ba7239ed72d6ce3b770a52fe3b05a78a
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-07-04 22:30:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.18%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        // if(n==0)
-        //     return double(1);
-        // double p =1.0;
-        // if(n>0){
-        // for(int i=0;i<n;i++)
-        //     p = p*x;
-        // }
-        // else{
-        // for(int i=0;i<abs(n);i++)
-        //     p = p*x;
-        // p = 1.0/p;
-        // }
-        return pow(x,n);
-    }
+      double myPow(double x, int n) {
+      double ans = 1.0;
+      long long nn = n;
+      if (nn < 0) nn = -1 * nn;
+      while (nn) {
+        if (nn % 2) {
+          ans = ans * x;
+          nn = nn - 1;
+        } else {
+          x = x * x;
+          nn = nn / 2;
+        }
+      }
+      if (n < 0) ans = (double)(1.0) / (double)(ans);
+      return ans;
+}
 };
