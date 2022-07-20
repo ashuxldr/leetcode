7c2dd899c311cf74d1102f2d2b43f0da32ef2173
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-07-21 03:14:12 +0530

    Time: 3 ms (83.44%), Space: 6.9 MB (16.47%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    string removeOuterParentheses(string s) {
-        string st="";int c=0;
-        for(char i:s){
-            if(i=='(' and c++ >0)
-                st+=i;
-            else if(i==')' and c-- >1)
-                st+=i;
+     string removeOuterParentheses(string s) {
+        stack<char> str;
+        string output = "";
+        int j=1;
+        for(int i=0;i<s.length();i++){
+            if(!str.empty() && s[i] == ')' && str.top() == '('){
+                // str.push(s[i]);
+                str.pop();
+            }
+            else{
+                str.push(s[i]);
+            }
+            if(str.empty()){
+                output += s.substr(j,i-j);
+                // cout << output<< endl;
+                j = i+2;
+            }
         }
-        return st;
+        return output;
     }
 };
