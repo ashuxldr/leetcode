8f9f5f77935bcb9fd6373c2c3a8555fad645f7a8
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-08-08 21:38:26 +0530

    Time: 523 ms (33.65%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,15 +1,31 @@
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         vector<int> tail;
+//         for(int num : nums){
+//             if(tail.empty() or num > tail.back()){
+//                 tail.push_back(num);
+//             }
+//             else{
+//                 tail[(lower_bound(tail.begin(), tail.end(), num) - tail.begin())] = num;
+//             }
+//         }
+//         return tail.size();
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> tail;
-        for(int num : nums){
-            if(tail.empty() or num > tail.back()){
-                tail.push_back(num);
-            }
-            else{
-                tail[(lower_bound(tail.begin(), tail.end(), num) - tail.begin())] = num;
+        const int n = nums.size();
+        vector<int> dp(n, 1);
+        
+        for(int i{1}; i<n; i++){
+            for(int j{0}; j<i; j++){
+                if(nums[i] > nums[j]) dp[i] = max(dp[i], dp[j]+1);
             }
         }
-        return tail.size();
+        
+        return *max_element(dp.begin(), dp.end());
     }
 };
