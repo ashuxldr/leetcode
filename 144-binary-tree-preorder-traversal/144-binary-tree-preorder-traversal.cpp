0c2a842b1b136c0a293ae391002a210140e8a809
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-08-12 19:42:38 +0530

    Time: 4 ms (41.81%), Space: 8.4 MB (42.22%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,19 +11,64 @@
  */
 class Solution {
 public:
-    void pre(TreeNode* root, vector<int> &res){
-        if(root==NULL)
-        return;
     
-        res.push_back(root->val);
-        pre(root->left, res);
-        pre(root->right, res);
-    }
-
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
-        pre(root, res);
+        if(root==NULL)
+            return res;
+        stack<TreeNode*> st;
+        while(true){
+            if(root){
+                st.push(root);
+                res.push_back(root->val);
+                root = root->left;
+            }
+            else{
+            if(st.empty()) break;
+            root = st.top();
+            st.pop();
+                root=root->right;
+                
+}
+        }
         return res;
-        
     }
+    
+    
+//     ITERATIVE-2
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> res;
+    //     if(root==NULL)
+    //         return res;
+    //     stack<TreeNode*> st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         TreeNode* curr = st.top();
+    //         res.push_back(curr->val);
+    //         st.pop();
+    //         if(curr->right)
+    //             st.push(curr->right);
+    //         if(curr->left)
+    //             st.push(curr->left);
+    //     }
+    //     return res;
+    // }
+    
+    
+//     RECURSIVE
+//     void pre(TreeNode* root, vector<int> &res){
+//         if(root==NULL)
+//         return;
+    
+//         res.push_back(root->val);
+//         pre(root->left, res);
+//         pre(root->right, res);
+//     }
+
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         pre(root, res);
+//         return res;
+        
+//     }
 };
