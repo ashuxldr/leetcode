dd4e243649cca5e173604a449a1b1df3fdd7890b
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-08-17 16:02:18 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (46.09%) - LeetHub

--- 806-number-of-lines-to-write-string/806-number-of-lines-to-write-string.cpp
+++ 806-number-of-lines-to-write-string/806-number-of-lines-to-write-string.cpp
@@ -1,15 +1,22 @@
+// TIME: O(N)
+// SPACE: O(1)
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-        int lines = 0;
-        int curr = 0;
+        // to store count of lines and curr width of pixels         
+        int lines = 0,curr = 0;
         for(int i=0;i<s.size();i++){
+            // checking if adding a character will exceed line             
             if(curr + widths[s[i]-'a'] > 100){
+                // increase line                 
                 ++lines;
+                // set current line to 0 (starting of new line)                 
                 curr = 0;
             }
+            // add pixels of character             
             curr = curr + widths[s[i]-'a'];
         }
+        // add 1 to lines for the current line and return the pixels
         return {lines+1, curr};
     }
 };
