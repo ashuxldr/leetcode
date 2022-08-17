d1f1e274a61d4a75430c00d4995960bf3b3d6f46
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-08-17 12:19:01 +0530

    Time: 4 ms (71.25%), Space: 8.7 MB (30.73%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
+        // array for storing morse code         
         vector<string> morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    
-        map<string,int> m;
-        int c=0;
-    for(auto it:words){
+        
+        // unordered map for storing unique values(no ordering is required)         
+        unordered_map<string,int> m;
+        
+        // loop through array for words         
+        for(auto it:words){
+        
+        // storing morse code 
         string r = "";
         for(auto i:it){
+            // i-'a' -> 0 for char('a') which will refer to 0 index 
+            // then access morse[0] for required change             
             r = r+morse[i-'a'];
         }
+        // store the unique morse codes (set can also be used)         
         m[r]++;
     }
-        for(auto itr:m)
-            c++;
-        return c;
+        // return size of map         
+        return m.size();
     }
 };
