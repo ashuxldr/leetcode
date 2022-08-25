6fd311a91195963b47927ff341e23acefc4aaa49
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-08-25 12:38:53 +0530

    Time: 34 ms (35.87%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -5,12 +5,13 @@ public:
         for(auto ch:magazine)
             m[ch]++;
         
-        for(auto ch:ransomNote)
-            m[ch]--;
-        
-        for(auto it:m)
-            if(it.second<0)
+        for(auto ch:ransomNote){
+            if(m[ch]==0)
                 return false;
+            m[ch]--;
+
+        }
+ 
         return true;
     }
 };
