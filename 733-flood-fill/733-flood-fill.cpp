48726ebad873ed97a55ba940f2ac949916829d01
Ashutosh Jaiswal <ashuxldr@gmail.com>
2022-08-29 23:14:22 +0530

    Time: 7 ms (95.53%), Space: 14.1 MB (34.17%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,25 +1,25 @@
-
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {         
-        int color = image[sr][sc];
-        if (color == newColor) return image;
-        dfs(&image, sr, sc, newColor, color);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {         
+        if(color==image[sr][sc])
+            return image;
+        dfs(image,sr,sc,color);
         return image;
     }
-
 private:
-    void dfs(vector<vector<int>> *image, int r, int c, int newColor, int color) {
-        static const vector<pair<int, int>> directions{{-1,  0}, { 1,  0},
-                                                       { 0,  1}, { 0, -1}};
-        if (r < 0 || r >= image->size() ||
-            c < 0 || c >= (*image)[0].size() ||
-            (*image)[r][c] != color) {
-            return;
-        }
-        (*image)[r][c] = newColor;
-        for (const auto& d : directions) {
-            dfs(image, r + d.first, c + d.second, newColor, color);
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color){
+        int m = image.size();
+        int n = image[0].size();
+        int oldColor = image[sr][sc];
+        image[sr][sc] = color;
+        vector<int> dr = {0,1,0,-1};
+        vector<int> dc = {1,0,-1,0};
+        for(int i=0;i<4;i++){
+            int r = sr+dr[i];
+            int c = sc+dc[i];
+            if(r>=m or r<0 or c<0 or c>=n or oldColor!=image[r][c])
+            continue;
+            dfs(image,r,c,color);
         }
     }
 };
